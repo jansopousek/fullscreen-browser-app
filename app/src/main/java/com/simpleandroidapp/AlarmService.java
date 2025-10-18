bbf764a92a9da69331d7ec9363bdb4c00d6bb4d9
Jan Sopousek <jan.sopousek@gmail.com>
2025-10-18 19:35:32 +0700

    fix alarm2

--- app/src/main/java/com/simpleandroidapp/AlarmService.java
+++ app/src/main/java/com/simpleandroidapp/AlarmService.java
@@ -16,6 +16,7 @@ import androidx.core.app.NotificationCompat;
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;       
 import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
